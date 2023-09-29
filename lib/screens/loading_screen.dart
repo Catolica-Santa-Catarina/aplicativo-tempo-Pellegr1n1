6a91eae54575548285a9d1c945de144b68a259c0
Pellegr1n1 <leandropellegrini391@gmail.com>
2023-09-28 22:02:25 -0300

    implementação json

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:developer';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:tempo_template/services/location_services.dart';
@@ -18,10 +18,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     log("Localização Atual: Lat: ${location.latitude}, Long ${location.longitute}");
   }
 
+  Future<void> getWeatherData() async {
+    var url = Uri.parse(
+        "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22");
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      log(data);
+    } else {
+      log(response.statusCode.toString());
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     getLocation();
+    getWeatherData();
   }
 
   @override
